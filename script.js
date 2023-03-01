ef612a27be48d8b953b6de57a6e260645f1ecc5d
Ondra Svec <ondrasvecveslo@gmail.com>
2023-03-01 09:54:39 +0100

    Add function adding two numbers together

--- script.js
+++ script.js
@@ -2,5 +2,9 @@
 //On digit button press, current operand becomes that digit/s
 //When an operation button is pressed, the previous operand takes the form
 //of the current operand + the operation pressed
+
+function add(num1, num2) {
+    return num1 + num2;
+}
 //When the evaluation button is pressed, the previous operand is null
 //and the current operand shows the result of the operation
