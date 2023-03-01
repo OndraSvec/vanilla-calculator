48010604020daa3e1ce452e7b9e12b5291a02a8f
Ondra Svec <ondrasvecveslo@gmail.com>
2023-03-01 09:53:47 +0100

    Add pseudocode for the calculator functionality

---
+++ script.js
@@ -0,0 +1,6 @@
+//On first load, previous operand is null and current operand is 0
+//On digit button press, current operand becomes that digit/s
+//When an operation button is pressed, the previous operand takes the form
+//of the current operand + the operation pressed
+//When the evaluation button is pressed, the previous operand is null
+//and the current operand shows the result of the operation
