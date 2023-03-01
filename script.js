025fecf53b7cf7731589a10fd1756bda7605743b
Ondra Svec <ondrasvecveslo@gmail.com>
2023-03-01 10:14:00 +0100

    Add raise to power function

--- script.js
+++ script.js
@@ -19,11 +19,17 @@ function divide(num1, num2) {
     return num1 / num2;
 }
 
+function raiseToPow(num1, num2) {
+    return Math.pow(num1, num2);
+}
+console.log(raiseToPow(2, 3));
+
 function operate(operator, num1, num2) {
     return operator === "+" ? add(num1, num2)
             : operator === "-" ? subtract(num1, num2)
             : operator === "*" ? multiply(num1, num2)
-            : divide(num1, num2);
+            : operator === "/" ? divide(num1, num2)
+            : raiseToPow(num1, num2);
 }
 //When the evaluation button is pressed, the previous operand is null
 //and the current operand shows the result of the operation
